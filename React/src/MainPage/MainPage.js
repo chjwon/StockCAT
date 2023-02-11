0cd4fd38c6e2e14feaebbb4c7aed4a6fcb396954
Yun Ho Jung <YunHo.Jung@ibm.com>
2023-02-11 14:21:02 -0600

    modal

--- React/src/MainPage/MainPage.js
+++ React/src/MainPage/MainPage.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainPage.css"
-import {Grid} from '@mui/material';
+import {Grid, Modal, Typography} from '@mui/material';
 import BestPredictions from "./BestPredictions/BestPredictions";
 
+const ModalButton = ({clickCallback}) => {
+    return (
+        <div className="modalButton" onClick={() => {clickCallback()}}>
+            <Typography variant="h1" style={{alignSelf:"center", fontSize: "50px", padding:"70px 0"}}>
+                {"< Past Predictions"}
+            </Typography>
+        </div>
+    )
+}
+
 const MainPage = () => {
+    const [displayModal, setDisplayModal] = useState(false)
+    const handleClose = () => {
+        setDisplayModal(false)
+    }
+
     return (
         <div className="main">
+            {displayModal && <div className="main_bundle_modal_outer">
+                <div className="main_bundle_modal">
+                    <div id="bundle_modal_cancel_button" onClick={() => {handleClose()}}>Close</div>
+                </div>
+            </div>}
             <Grid container spacing={1}>
                 <Grid item xs={12} md={12}>
                     <div style={{width: "100%"}}>
@@ -15,8 +35,12 @@ const MainPage = () => {
                 <Grid item xs={12} md={12}>
                     <BestPredictions/>
                 </Grid>
+                <Grid item xs={6} md={6}>
+                    <div className="buttonContainerLeft">
+                        <ModalButton clickCallback={() => {setDisplayModal(true)}}></ModalButton>
+                    </div>
+                </Grid>
             </Grid>
-            
         </div>
     )
 }  
